87458d54162038ac3d0a24a4648f4e205c0afdc7
Franziska Otto <franziska.otto@gmx.at>
2023-07-08 16:10:57 +0200

    startet first task

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,41 @@
-console.log("Test");
+
+let selectAll = document.getElementById("all");
+commonNames(countries);
+
+function commonNames(countries) {
+  let countrieNames =  []
+  
+  for (let i = 0; i < countries.length; i++) {
+    const country = countries[i];
+    const name = country.name.common;
+    console.log(name)
+    countrieNames.push(name)
+  }
+  createOption(countrieNames, countries)
+}
+
+
+function createOption(nameArray, countries) {
+  nameArray.forEach((countryName) => {
+    let options = document.createElement("option");
+    options.textContent = countryName;
+
+    options.addEventListener("click", (e) => {
+      detailsOfSelectedCountry(countries)
+    })
+
+    //does not work 
+    selectAll.appendChild(options);
+    
+    
+    
+  });
+  return options
+  
+
+}
+
+function detailsOfSelectedCountry(names) {
+  console.log(countries)
+  
+}
