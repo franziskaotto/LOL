23b72f763b5ba2a3e8bc839454e40232997afc16
Franziska Otto <franziska.otto@gmx.at>
2023-07-13 21:10:46 +0200

    nearly finished 1 + 2 task

--- frontend/script.js
+++ frontend/script.js
@@ -5,9 +5,13 @@ const loadEvent = function() {
   
   let insertIdToBody = document.querySelector("body");
   insertIdToBody.id = "root"
+
   let all = document.getElementById("all")
-  getCommonNames(countries);
   let countryElement = document.getElementById("country")
+
+
+  getCommonNames(countries);
+  //getRestOfCountry(countries);
 
 
   function getCommonNames(inputCountries) {
@@ -16,59 +20,120 @@ const loadEvent = function() {
     
     inputCountries.forEach(country => {
       //console.log(country);
-      
-      
       //array
       let names = country["name"]
       countryNameList.push(names.common)
-
-      
-      
-
     });
     //createUnorderedList(countryNameList)
 
     createOptions(countryNameList)
-    console.log(countryNameList)
+    //console.log(countryNameList)
     
     return countryNameList
   }
 
   function createOptions(inputList) {
+    
     inputList.forEach(names => {
       let chooseName = document.createElement("option");
       chooseName.innerHTML = names;
-      chooseName.addEventListener("click", (e) => {
-        showOneCountry(inputList)
-      })
       all.appendChild(chooseName)
-      
+
+    });
+
+    all.addEventListener("change", (e) => {
+      showOneCountry(e.target.value)
     });
   }
 
-  function showOneCountry(inputList) {
-    dataHolder.innerHTML = "";
-    console.log("ShowOneCountry clicked");
-    dataHolder.appendChild(createOneCountry(inputList))
-
-  }
-
-  function createOneCountry(inputList) {
+  function showOneCountry(oneCountryName) {
+    //need to have it to clear the Page
+    countryElement.innerHTML = ""
+    countryElement.appendChild(createOneCountryFromObject(oneCountryName))
     
-    inputList.forEach(country => {
-      let region = document.createElement("h2")
-      let regionName = country["region"];
-      region.innerHTML = regionName
-      
-    });
+    
   }
 
+  function createOneCountryFromObject(countryName) {
+    let flag = [];                  //img
+    let commonName = [];            //h1
+    let region = [];                //h2
+    let subRegion = [];             //h3
+    let capitalCity = [];           //h4
 
+    
+    for(let country of countries) {
+      
+      if (country.name.common === countryName) {
+        //flag
+        let flags = country.flags.png;
+        flag.push(flags);
 
+        //commonName
+        let names = country.name.common;
+        commonName.push(names);
 
+        //region
+        let regionName = country.region;
+        region.push(regionName);
+        
+        //subRegion
+        let sub = country.subregion;
+        subRegion.push(sub);
 
+        //capital
+        let capital = country.capital[0];
+        capitalCity.push(capital);
+        
+      }
+      
+      
+    };
+    createFlagImageElement(flag);
+    createH1Element(commonName);
+    createH2Element(region);
+    createh3Element(subRegion);
+    createh4Element(capitalCity);
+    
+  }
 
+  function createFlagImageElement(flag) {
+    let flagElement = document.createElement("img");
+    flagElement.src = flag;
+    countryElement.appendChild(flagElement);
+  };
 
+  function createH1Element(commonName) {
+    let h1Element = document.createElement("h1");
+    h1Element.innerHTML = commonName;
+    countryElement.appendChild(h1Element);
+  };
+
+  function createH2Element(region) {
+    let h2Element = document.createElement("h2");
+    h2Element.innerHTML = "Region: " + region;
+    countryElement.appendChild(h2Element);
+  };
+
+  function createh3Element(sub) {
+    let h3Element = document.createElement("h3"); 
+    h3Element.innerHTML = "Subregion: " + sub;
+    countryElement.appendChild(h3Element);
+  }
+
+  function createh4Element(capital) {
+    let h4Element = document.createElement("h4");
+    h4Element.innerHTML = "Capital: " + capital;
+    countryElement.appendChild(h4Element);
+  }
+
+  /*
+  function noCountrySelected() {
+    let noCountry = document.createElement("div")
+    noCountry.innerHTML = "Select a country from the list";
+    countryElement.appendChild(noCountry)
+  }
+  */
 
 
 
