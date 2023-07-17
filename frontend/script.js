514cbf2e9d08378311e4113b842a4d229902417c
Anmariru <anna.rumpl@gmx.net>
2023-07-17 19:24:31 +0200

    second task is completed

--- frontend/script.js
+++ frontend/script.js
@@ -6,12 +6,24 @@ const loadEvent = function (){
 
 function listAllCountries (countriesArr){
   const selectionE = document.getElementById("all");
+  const details = document.getElementById("country");
 
   for (let country of countriesArr){
     selectionE.insertAdjacentHTML("beforeend", element("option", country.name.common));
   }
 
-  
+  selectionE.addEventListener("input", (e) =>{
+    const selectedCountry = countriesArr.find(country => country.name.common === e.target.value);
+    details.innerHTML = "";
+
+    if (selectedCountry){
+      details.insertAdjacentHTML("beforeend", element("img", selectedCountry.flag))
+      details.insertAdjacentHTML("beforeend", element("h1", selectedCountry.name.common))
+      details.insertAdjacentHTML("beforeend", element("h2", selectedCountry.region))
+      details.insertAdjacentHTML("beforeend", element("h3", selectedCountry.subregion))
+      details.insertAdjacentHTML("beforeend", element("h3", selectedCountry.subregion))
+    }
+  })
 }
 
 function element (tag, inner) {
