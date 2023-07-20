1ca86a08a93731825781dc43d600a4c5a262a4de
Anmariru <anna.rumpl@gmx.net>
2023-07-20 19:44:21 +0200

    hidden button to largest population is added

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,6 @@
 
 const LISTOFCOUNTRIES = countries; //connection through index.html
 
-
 const loadEvent = function (){
   listAllCountries (LISTOFCOUNTRIES);
 }
@@ -10,6 +9,7 @@ function listAllCountries (countriesArr){
   const selectionE = document.getElementById("all");
   const detailsE = document.getElementById("country");
   const populationBtn = document.getElementById("population");
+  populationBtn.style.display = "none";
 
   selectionE.innerHTML = "<option>--Select a country from the list--</option>";
   fillSelection(countriesArr, selectionE);
@@ -20,12 +20,12 @@ function listAllCountries (countriesArr){
 
     if (selectedCountry){
       fillPage(selectedCountry, detailsE);
+      populationBtn.style.display = "inline";
     }
     //need to add event listener for button
     populationBtn.addEventListener("click", (e) =>{
-      let largestCountry = listLargestPopulation(selectedCountry, detailsE, selectionE);
+      let largestCountry = listLargestPopulation(selectedCountry, detailsE);
       selectionE.innerHTML = `<option>${largestCountry}</option>`;
-      fillSelection(countriesArr, selectionE);
       listAllCountries (LISTOFCOUNTRIES)
     })
   })
@@ -34,41 +34,6 @@ function listAllCountries (countriesArr){
 function fillSelection (countriesArr, selectionE){
   for (let country of countriesArr){
     selectionE.insertAdjacentHTML("beforeend", element("option", country.name.common));
-  }
-}
-
-function listLargestPopulation (country, detailsE, selectionE){
-  if (country.hasOwnProperty("borders")){ //if there are neighbouring countries, start the search
-    let largestNeighbour = getLargestNeighbour (country);
-    fillPage(largestNeighbour, detailsE);
-    return largestNeighbour.name.common;
-  }
-  else  {
-    detailsE.innerHTML = "";
-    detailsE.insertAdjacentHTML("beforeend", element("h1", "Country has no neighbouring countries."));
-    
-  }
-}
-
-function getLargestNeighbour (country){
-  for (let neighbourCCA3 of country.borders){ //i get the cca3 of the borders array
-    let neededCountry = findCountry(neighbourCCA3);
-    let tempPopulation = neededCountry.population;
-    let largestNeighbour = neededCountry;
-    
-    if (tempPopulation < neededCountry.population){
-      largestNeighbour = neededCountry;
-      tempPopulation = neededCountry.population
-    }
-    return largestNeighbour;
-  }
-}
-
-function findCountry (cca3){
-  for (let country of LISTOFCOUNTRIES){
-    if (country.cca3 === cca3){ // i search with the given cca3 in the list of countries and return the needed country
-      return country;
-    }
   }
 }
 
@@ -94,5 +59,43 @@ function fillPage (selectedCountry, detailsE){
     detailsE.insertAdjacentHTML("beforeend", element("h4", selectedCountry.capital[0]))
   }
 }
+
+function listLargestPopulation (country, detailsE){
+  if (country.hasOwnProperty("borders")){ //if there are neighbouring countries, start the search
+    let largestNeighbour = getLargestNeighbour (country);
+    fillPage(largestNeighbour, detailsE);
+    return largestNeighbour.name.common;
+  }
+  else  {
+    detailsE.innerHTML = "";
+    detailsE.insertAdjacentHTML("beforeend", element("h1", "Country has no neighbouring countries."));
+  }
+}
+
+function getLargestNeighbour (country){
+  for (let neighbourCCA3 of country.borders){ //i get the cca3 of the borders array
+    let neededCountry = findCountry(neighbourCCA3);
+    let tempPopulation = neededCountry.population;
+    let largestNeighbour = neededCountry;
+    
+    if (tempPopulation < neededCountry.population){
+      largestNeighbour = neededCountry;
+      tempPopulation = neededCountry.population
+    }
+    return largestNeighbour;
+  }
+}
+
+function findCountry (cca3){
+  for (let country of LISTOFCOUNTRIES){
+    if (country.cca3 === cca3){ // i search with the given cca3 in the list of countries and return the needed country
+      return country;
+    }
+  }
+}
+
+
+
+
 
 window.addEventListener("load", loadEvent);
